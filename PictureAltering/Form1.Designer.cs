335d22f92bec2e05c8a8e412a424cefcc7ede040
Eric Talviste <Eric.Talviste@tptlive.ee>
2018-06-07 15:41:38 +0300

    crreation

--- PictureAltering/Form1.Designer.cs
+++ PictureAltering/Form1.Designer.cs
@@ -28,13 +28,230 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PBX_Input = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.TLS_ChooseFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.TLS_ChooseFolder = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolTriangle = new System.Windows.Forms.ToolStripButton();
+            this.toolCircle = new System.Windows.Forms.ToolStripButton();
+            this.toolDiamond = new System.Windows.Forms.ToolStripButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.PBX_Output = new System.Windows.Forms.PictureBox();
+            this.Input = new System.Windows.Forms.GroupBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.TSL_Size = new System.Windows.Forms.ToolStripStatusLabel();
+            this.L_ModeStatus = new System.Windows.Forms.Label();
+            this.LBX_Pictures = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PBX_Input)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBX_Output)).BeginInit();
+            this.Input.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PBX_Input
+            // 
+            this.PBX_Input.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.PBX_Input.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PBX_Input.Location = new System.Drawing.Point(12, 25);
+            this.PBX_Input.Name = "PBX_Input";
+            this.PBX_Input.Size = new System.Drawing.Size(286, 254);
+            this.PBX_Input.TabIndex = 9;
+            this.PBX_Input.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripSeparator1,
+            this.toolTriangle,
+            this.toolCircle,
+            this.toolDiamond});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.toolStrip1.Size = new System.Drawing.Size(876, 32);
+            this.toolStrip1.TabIndex = 10;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TLS_ChooseFile,
+            this.TLS_ChooseFolder});
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(56, 29);
+            this.toolStripLabel1.Text = "File";
+            // 
+            // TLS_ChooseFile
+            // 
+            this.TLS_ChooseFile.Name = "TLS_ChooseFile";
+            this.TLS_ChooseFile.Size = new System.Drawing.Size(252, 30);
+            this.TLS_ChooseFile.Text = "Choose Picture";
+            this.TLS_ChooseFile.Click += new System.EventHandler(this.TLS_ChooseFile_Click);
+            // 
+            // TLS_ChooseFolder
+            // 
+            this.TLS_ChooseFolder.Name = "TLS_ChooseFolder";
+            this.TLS_ChooseFolder.Size = new System.Drawing.Size(252, 30);
+            this.TLS_ChooseFolder.Text = "Choose Folder";
+            this.TLS_ChooseFolder.Click += new System.EventHandler(this.TLS_ChooseFolder_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 32);
+            // 
+            // toolTriangle
+            // 
+            this.toolTriangle.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolTriangle.Image = ((System.Drawing.Image)(resources.GetObject("toolTriangle.Image")));
+            this.toolTriangle.ImageTransparentColor = System.Drawing.Color.White;
+            this.toolTriangle.Name = "toolTriangle";
+            this.toolTriangle.Size = new System.Drawing.Size(28, 29);
+            this.toolTriangle.Text = "toolTriangle";
+            this.toolTriangle.ToolTipText = "TRIANGLE";
+            this.toolTriangle.Click += new System.EventHandler(this.toolTriangle_Click);
+            // 
+            // toolCircle
+            // 
+            this.toolCircle.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolCircle.Image = ((System.Drawing.Image)(resources.GetObject("toolCircle.Image")));
+            this.toolCircle.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolCircle.Name = "toolCircle";
+            this.toolCircle.Size = new System.Drawing.Size(28, 29);
+            this.toolCircle.Text = "toolCircle";
+            this.toolCircle.ToolTipText = "CIRCLE";
+            this.toolCircle.Click += new System.EventHandler(this.toolCircle_Click);
+            // 
+            // toolDiamond
+            // 
+            this.toolDiamond.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolDiamond.Image = ((System.Drawing.Image)(resources.GetObject("toolDiamond.Image")));
+            this.toolDiamond.ImageTransparentColor = System.Drawing.Color.White;
+            this.toolDiamond.Name = "toolDiamond";
+            this.toolDiamond.Size = new System.Drawing.Size(28, 29);
+            this.toolDiamond.Text = "toolStripButton1";
+            this.toolDiamond.Click += new System.EventHandler(this.toolDiamond_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.PBX_Output);
+            this.groupBox1.Location = new System.Drawing.Point(552, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 291);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output";
+            // 
+            // PBX_Output
+            // 
+            this.PBX_Output.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.PBX_Output.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PBX_Output.Location = new System.Drawing.Point(12, 25);
+            this.PBX_Output.Name = "PBX_Output";
+            this.PBX_Output.Size = new System.Drawing.Size(286, 254);
+            this.PBX_Output.TabIndex = 10;
+            this.PBX_Output.TabStop = false;
+            // 
+            // Input
+            // 
+            this.Input.Controls.Add(this.PBX_Input);
+            this.Input.Location = new System.Drawing.Point(235, 49);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(311, 291);
+            this.Input.TabIndex = 11;
+            this.Input.TabStop = false;
+            this.Input.Text = "Input";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TSL_Size});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 398);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(876, 30);
+            this.statusStrip1.TabIndex = 15;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // TSL_Size
+            // 
+            this.TSL_Size.Name = "TSL_Size";
+            this.TSL_Size.Size = new System.Drawing.Size(43, 25);
+            this.TSL_Size.Text = "Size";
+            // 
+            // L_ModeStatus
+            // 
+            this.L_ModeStatus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.L_ModeStatus.Location = new System.Drawing.Point(235, 349);
+            this.L_ModeStatus.Name = "L_ModeStatus";
+            this.L_ModeStatus.Size = new System.Drawing.Size(629, 34);
+            this.L_ModeStatus.TabIndex = 14;
+            this.L_ModeStatus.Text = "Mode:";
+            // 
+            // LBX_Pictures
+            // 
+            this.LBX_Pictures.FormattingEnabled = true;
+            this.LBX_Pictures.ItemHeight = 22;
+            this.LBX_Pictures.Location = new System.Drawing.Point(29, 49);
+            this.LBX_Pictures.Name = "LBX_Pictures";
+            this.LBX_Pictures.Size = new System.Drawing.Size(200, 334);
+            this.LBX_Pictures.TabIndex = 13;
+            this.LBX_Pictures.SelectedIndexChanged += new System.EventHandler(this.LBX_Pictures_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(876, 428);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.L_ModeStatus);
+            this.Controls.Add(this.LBX_Pictures);
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Altering Images";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PBX_Input)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PBX_Output)).EndInit();
+            this.Input.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox PBX_Input;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripLabel1;
+        private System.Windows.Forms.ToolStripMenuItem TLS_ChooseFile;
+        private System.Windows.Forms.ToolStripMenuItem TLS_ChooseFolder;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolTriangle;
+        private System.Windows.Forms.ToolStripButton toolCircle;
+        private System.Windows.Forms.ToolStripButton toolDiamond;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox PBX_Output;
+        private System.Windows.Forms.GroupBox Input;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel TSL_Size;
+        private System.Windows.Forms.Label L_ModeStatus;
+        private System.Windows.Forms.ListBox LBX_Pictures;
     }
 }
 
--- PictureAltering/Form1.cs
+++ PictureAltering/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,286 @@ namespace PictureAltering
 {
     public partial class Form1 : Form
     {
+
+        public string[] files;
+
+        Image Pilt;
+        Bitmap Piltmap1, Piltmap2;
+        int PiltW, PiltH;
+
         public Form1()
         {
             InitializeComponent();
+
+            DefaultPicturePack();
+
+            PBX_Input.SizeMode = PictureBoxSizeMode.Zoom;
+            PBX_Output.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        #region TOOLSTRIP TOGGLE-BUTTONS
+        //Array for shapes
+        private ToolStripButton[] Shapes;
+
+        // Allow only one shape selection at a time.
+        private void Shapes_Click(object sender, EventArgs e)
+        {
+            SelectToolStripButton(sender as ToolStripButton, Shapes);
+        }
+
+        // Select the indicated button and deselect the others.
+        private void SelectToolStripButton(ToolStripButton selected_button, ToolStripButton[] buttons)
+        {
+            foreach (ToolStripButton test_button in buttons)
+            {
+                test_button.Checked = (test_button == selected_button);
+            }
+        }
+        #endregion
+
+        //Choose ONE file 
+        private void TLS_ChooseFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog OFD = new OpenFileDialog())
+            {
+                OFD.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF";
+
+                if (OFD.ShowDialog() == DialogResult.OK)
+                {
+                    if (LBX_Pictures.Items.Contains(Path.GetFileName(OFD.FileName)))
+                    {
+                        return;
+                    }
+
+                    else
+                    {
+                        string chosenFile = OFD.FileName;
+                        files = files.Concat(new string[] { chosenFile }).ToArray();
+                        LBX_Pictures.Items.Add(Path.GetFileName(chosenFile));
+                    }
+                }
+            }
+        }
+
+        //Add Pictures to listbox from folder
+        private void TLS_ChooseFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                LBX_Pictures.Items.Clear();
+
+                files = Directory.GetFiles(FBD.SelectedPath);
+
+                foreach (var file in files)
+                {
+                    LBX_Pictures.Items.Add(Path.GetFileName(file));
+                }
+
+            }
+        }
+
+        //Add Default Pictures
+        private void DefaultPicturePack()
+        {
+            files = Directory.GetFiles("DefaultCats");
+
+            foreach (var file in files)
+            {
+                LBX_Pictures.Items.Add(Path.GetFileName(file));
+            }
+        }
+
+        //What happens if a different picture is chosen
+        private void LBX_Pictures_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (toolTriangle.Checked)
+                {
+                    Triangle();
+                }
+
+                if (toolCircle.Checked)
+                {
+                    Circle();
+                }
+
+                if (toolDiamond.Checked)
+                {
+                    Diamond();
+                }
+
+                StandBy();
+
+                TSL_Size.Text = "Size: " + PBX_Input.Image.Size.ToString();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void toolTriangle_Click(object sender, EventArgs e)
+        {
+            Triangle();
+        }
+        private void toolCircle_Click(object sender, EventArgs e)
+        {
+            Circle();
+        }
+        private void toolDiamond_Click(object sender, EventArgs e)
+        {
+            Diamond();
+        }
+
+        //Show picture in INPUT
+        private void StandBy()
+        {
+            try
+            {
+                string ImagePath = files[LBX_Pictures.SelectedIndex];
+                PBX_Input.Image = new Bitmap(ImagePath);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+        }
+
+        //Go through triangle logic -> OUTPUT
+        private void Triangle()
+        {
+            StandBy();
+
+            L_ModeStatus.Text = "Mode: Triangle";
+
+            if (PBX_Input.Image != null)
+            {
+                Piltmap1 = new Bitmap(PBX_Input.Image);
+
+                PiltW = PBX_Input.Image.Width;
+                PiltH = PBX_Input.Image.Height;
+
+                Piltmap2 = new Bitmap(PiltW, PiltH);
+
+                for (int y = 0; y < PiltH; y++)
+                {
+                    int laius = (int)((float)y * PiltW / PiltH);
+                    int dx = (PiltW - laius) / 2;
+
+                    for (int x = 0; x < laius; x++)
+                    {
+                        int xx = (int)((float)x * PiltW / laius);
+                        Color c = Piltmap1.GetPixel(xx, y);
+                        Piltmap2.SetPixel(dx + x, y, c);
+                    }
+                }
+                PBX_Output.Image = Piltmap2;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        //Init items
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Shapes = new ToolStripButton[]
+          {
+                toolTriangle,
+                toolCircle,
+                toolDiamond
+          };
+
+            foreach (ToolStripButton B in Shapes) { B.Click += Shapes_Click; }
+        }
+
+        private void Circle()
+        {
+            try
+            {
+                StandBy();
+
+                L_ModeStatus.Text = "Mode: Circle";
+
+                if (PBX_Input.Image != null)
+                {
+                    Piltmap1 = new Bitmap(PBX_Input.Image);
+
+                    PiltW = PBX_Input.Image.Width;
+                    PiltH = PBX_Input.Image.Height;
+
+                    Piltmap2 = new Bitmap(PiltW, PiltH);
+
+                    float a = PiltW / 2;
+                    float b = PiltH / 2;
+
+                    for (int y = 0; y < PiltH; y++)
+                    {
+                        double yy = b - y;
+                        double xx = a / b * Math.Sqrt(b * b - yy * yy);
+                        int dx = (int)(b - xx);
+                        int laius = (int)(2 * xx);
+
+                        for (int x = 0; x < laius; x++)
+                        {
+                            int xxx = x * PiltW / laius;
+                            Color c = Piltmap1.GetPixel(xxx, y);
+                            Piltmap2.SetPixel(dx + x, y, c);
+                        }
+                    }
+                    PBX_Output.Image = Piltmap2;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+        }
+
+        private void Diamond()
+        {
+            try
+            {
+                StandBy();
+
+                L_ModeStatus.Text = "Mode: Diamond";
+
+                Bitmap Piltmap1 = new Bitmap(PBX_Input.Image);
+
+                PiltW = Piltmap1.Width;
+                PiltH = Piltmap1.Height;
+                PointF[] x =
+                {
+                    new PointF(PiltW * 0.5f, 0),
+                    new PointF(0, PiltH * 0.4f),
+                    new PointF(PiltW, PiltH * 0.6f)
+                };
+
+                Bitmap Piltmap2 = new Bitmap(
+                (int)Piltmap1.Width,
+                (int)Piltmap1.Height);
+
+                Graphics gr_dest = Graphics.FromImage(Piltmap2);
+
+                gr_dest.DrawImage(Piltmap1, x);
+
+                PBX_Output.Image = Piltmap2;
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
         }
     }
 }
